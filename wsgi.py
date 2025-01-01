c0d51bfcae83ee3b7aa01edf974c309ee202c314
NoobAndCoding <167626518+NoobAndCoding@users.noreply.github.com>
2025-01-01 12:39:34 +0200

    Update wsgi.py
    Added readability notes to the WSGI server initialization script

--- wsgi.py
+++ wsgi.py
@@ -4,5 +4,7 @@ from app import app
 
 # -- Run the WSGI server --
 if __name__ == "__main__":
+    # -- Define the server port --
     port = int(os.environ.get("PORT", 10000))
+    # -- Initialize the WSGI server --
     app.run(host = "0.0.0.0", port = port)
